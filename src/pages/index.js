c0e333f9ac25b47d2ef402de41cfe84edaf5c92d
James Belchamber <james@belchamber.com>
2021-11-20 22:03:01 +0000

    prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,8 +22,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="An Integrated Tech Industry Foundations Course">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
